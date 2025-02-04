5c9de66d22f4571fb0980079f82d9f24915255d4
Thabang <156672986+ThabangLenonyana@users.noreply.github.com>
2025-02-04 11:50:13 +0000

    hotfix 4

--- src/app.py
+++ src/app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 from pages.Home import main
 
-st.set_page_config(
-    page_title="IELTS Tutor",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
 if __name__ == "__main__":
     main()
